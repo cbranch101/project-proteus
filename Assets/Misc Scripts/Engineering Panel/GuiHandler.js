7b606caf1c2343dc0dc4961f0314f23623dd10da
Clay Branch <clay@movementstrategy.com>
2012-10-26 17:45:19 -0400

    pulling changes

--- Assets/Misc Scripts/Engineering Panel/GuiHandler.js
+++ Assets/Misc Scripts/Engineering Panel/GuiHandler.js
@@ -3,14 +3,12 @@
 public var GuiScript : MyGui;
 var firstPersonController : GameObject;
 var mainCamera : MouseLook;
-var launcher : MissileLauncher;
 
 function Update () {
 	
 	
 	if(Input.GetKeyDown('e')) {
 		mainCamera.enabled = mainCamera.enabled == true ? false : true;
-		launcher.enabled = launcher.enabled == true ? false : true;
 		var allComponents = firstPersonController.GetComponents(MonoBehaviour);
 		for(var component : MonoBehaviour in allComponents) {
 			component.enabled = component.enabled == true ? false : true;
