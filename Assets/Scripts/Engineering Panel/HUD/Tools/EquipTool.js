c83476686b3a78c11d292185ee60fe69fd87e798
Clay Branch <clay@movementstrategy.com>
2012-12-11 07:44:51 -0500

    starting weapon script

---
+++ Assets/Scripts/Engineering Panel/HUD/Tools/EquipTool.js
@@ -0,0 +1,17 @@
+class EquipTool extends GameTool {
+	
+	protected var equippable = true;
+	
+	function toggleEquipped() {
+		
+	}
+	
+	function attachToMainCamera() {
+		
+	}
+	
+	function attachToInventory() {
+		
+	}
+		
+}
